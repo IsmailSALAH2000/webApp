8d5197c3a21d5826e473852615514d64c5c6c74f
hitanox <102423395+hitanox@users.noreply.github.com>
2023-05-02 22:44:47 +0200

    Add files via upload

--- app/model/User.php
+++ app/model/User.php
@@ -45,15 +45,39 @@ class User {
         return 1;
     }
 
-    public function ajoutUtilisateur($login, $mdpHash) {
-        if($this->loginDisponible($login)) {
+    public function mailDisponible($email) {
+        $data['email'] = $email;
+        $user = $this->select($data);
+        if($user) {
+            return 0;
+        }
+        return 1;
+    }
+
+    public function ajoutUtilisateur($login, $mdpHash, $email) {
+        if($this->loginDisponible($login) && $this->mailDisponible($email)) {
             $data['login'] = $login;
             $data['mdpHash'] = $mdpHash;
+            $data['email'] = $email;
             if(is_array($this->insert($data))) {
                 return 1;
             }
         }
-        return 0; //login non dispo ou erreur lors de l'ajout
+        return 0; //login ou mail non dispo ou erreur lors de l'ajout
+    }
+
+    public function ajoutUtilisateur2($login, $mdpHash, $email, $nom, $prenom) {
+        if($this->loginDisponible($login) && $this->mailDisponible($email)) {
+            $data['login'] = $login;
+            $data['mdpHash'] = $mdpHash;
+            $data['email'] = $email;
+            $data['nom'] = $nom;
+            $data['prenom'] = $prenom;
+            if(is_array($this->insert($data))) {
+                return 1;
+            }
+        }
+        return 0; //login ou mail non dispo ou erreur lors de l'ajout
     }
 
     public function modifierLoginUtilisateur($login, $nouveauLogin) {
