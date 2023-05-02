2ca1d871bf053186edb02c8e42d7e441461fadcd
hitanox <102423395+hitanox@users.noreply.github.com>
2023-05-02 20:52:19 +0200

    Add files via upload

--- app/controllers/QcmController.php
+++ app/controllers/QcmController.php
@@ -5,16 +5,27 @@ class QcmController
     use Controller;
     public function index($data=[])
     {
-        
-            $qcm = new QcmModel("./qcm.xml");
-            $qcm = $qcm->getQuestions();
-            show($qcm);
-            header('Content-Type: application/json');
-            echo json_encode($qcm);
-            
-        
+        $qcm = new QcmModel;
+        $donneesQCM = $qcm->getQCM('qcm'); //ici qcm est l'id du qcm
 
-        $this->view("qcm", $data);
+        //exemple d'utilisation
+        echo '<p>Type : '.$donneesQCM['type'].'</p><br/>';
+        foreach($donneesQCM['questions'] as $question) {
+            echo '<p>'.$question['text'].'</p>';
+            foreach($question['choices'] as $choice) {
+                echo '<p>'.$choice['text'].' '.$choice['correct'].'</p>'; //$choices['correct'] contient true si c'est une bonne réponde, sinon rien
+            }
+            echo '<br/>';
+        }
+
+        $qcm->ajoutQCM("oui", $donneesQCM);
+
+        $qcm->supprimeQCM("oui");
+        
+        $listeQCM = $qcm->getAllQCM();
+        foreach($listeQCM as $nomQCM) {
+            echo '<p>t : '.$nomQCM.'</p>'; 
+        }
     }
 }
 
