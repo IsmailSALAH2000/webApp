dd0d27a537eb179db43f2e21b01b82558d19d356
hitanox <102423395+hitanox@users.noreply.github.com>
2023-05-02 20:51:42 +0200

    Add files via upload

--- app/model/Cours.php
+++ app/model/Cours.php
@@ -4,6 +4,7 @@ class Cours {
     use Model;
     protected $table = "Cours";
 
+    //retourne le cours d'id $id, 0 s'il n'existe pas
     public function getCours($id) {
         $data['idCours'] = $id;
         $cours = $this->select($data);
@@ -13,6 +14,17 @@ class Cours {
         return 0;
     }
 
+    //retourne tous les cours de type $type, 0 s'il n'en existe pas
+    public function getCoursByType($type) {
+        $data['type'] = $type;
+        $cours = $this->select($data);
+        if($cours) {
+            return $cours;
+        }
+        return 0;
+    }
+
+    //ajoute un cours à la base de donnée, retourne 1 si l'ajout est effectué, 0 sinon
     public function ajoutCours($id, $titre, $description, $type, $chemin) {
         $data['idCours'] = $id;
         $data['titre'] = $titre;
@@ -25,11 +37,12 @@ class Cours {
         return 0;
     }
 
+    //ajoute le cours d'id $id de la base de donnée, retourne 1 si la suppression est effectuée, 0 sinon
     public function supprimerCours($id) {
         if(is_array($this->delete($id, 'idCours'))) {
             return 1;
         }
-        return 0;//erreur
+        return 0;
     }
 
 }
--- app/model/QcmModel.php
+++ app/model/QcmModel.php
@@ -1,26 +1,105 @@
 <?php
 class QcmModel {
   use Model;
-  private $xml_file;
-  
-  public function __construct($xml_file) {
-    $this->xml_file = $xml_file;
+
+  //retourne le qcm de nom $id ou null si il n'existe pas
+  public function getQCM($id) {
+    if($this->QCMExiste($id)) {
+      $xml = simplexml_load_file('../app/model/qcm/'.$id.'.xml');
+      $questions = array();
+      foreach ($xml->question as $question) {
+        $question_text = (string)$question->text;
+        $choices = array();
+        foreach ($question->choice as $choice) {
+          $choice_text = (string)$choice;
+          $is_correct = (string)$choice['correct'];
+          $choices[] = array('text' => $choice_text, 'correct' => $is_correct);
+        }
+        $questions[] = array('text' => $question_text, 'choices' => $choices);
+      }
+      return array('type' => $xml->type, 'questions' => $questions);
+    }
+    return null;
   }
 
-  public function getQuestions() {
-    $questions = array();
-    $xml = simplexml_load_file($this->xml_file);
-    foreach ($xml->question as $question) {
-      $question_text = (string)$question->text;
-      $choices = array();
-      foreach ($question->choices->choice as $choice) {
-        $choice_text = (string)$choice;
-        $is_correct = (string)$choice['correct'];
-        $choices[] = array('text' => $choice_text, 'correct' => $is_correct);
-      }
-      $questions[] = array('text' => $question_text, 'choices' => $choices);
+  //retourne le type du qcm de nom $id ou null si il n'existe pas
+  public function getTypeQCM($id) {
+    if($this->QCMExiste($id)) {
+      $xml = simplexml_load_file('../app/model/qcm/'.$id.'.xml');
+      return $xml->type;
     }
-    return $questions;
+    return null;
   }
+
+  //retourne une liste contenant le nom de tous les qcm disponibles
+  public function getAllQCM() {
+    $listeFichiers = scandir('../app/model/qcm');
+    $listeQCM = array();
+    foreach($listeFichiers as $nomFichier) {
+      $taille = strlen($nomFichier);
+      if($taille>4) { //évite de retourner . et ..
+        $nomQCM = substr($nomFichier, 0, $taille-4);
+        $listeQCM[] = $nomQCM;
+      }
+    }
+    return $listeQCM;
+  }
+
+  //ajoute le qcm avec pour nom $id et comme données celles contenues dans $data
+  //retourne 0 si l'ajout s'est bien passé, 1 sinon
+  //data est du type ('type' => val, 'questions' => array('text' => val, 'choices' => array('text' => val, 'correct' => val)))
+  public function ajoutQCM($id, $data) {
+    if(!$this->QCMExiste($id)) {
+      $dom = new DOMDocument('1.0', 'UTF-8');
+      $qcm = $dom->createElement('qcm');
+      $type = $dom->createElement('type', $data['type']);
+      $qcm->appendChild($type);
+
+      foreach($data['questions'] as $question) {
+        $question_xml = $dom->createElement('question');
+        $text = $dom->createElement('text', $question['text']);
+        $question_xml->appendChild($text);
+
+        foreach($question['choices'] as $choice) {
+          $choice_xml = $dom->createElement('choice', $choice['text']);
+          $correct = $dom->createAttribute('correct');
+          $correct->value = $choice['correct'];
+          $choice_xml->appendChild($correct);
+          $question_xml->appendChild($choice_xml);
+        }
+        $qcm->appendChild($question_xml);
+      }
+
+      $dom->appendChild($qcm);
+      $dom->formatOutput = true;
+      $dom->save('../app/model/qcm/'.$id.'.xml');//verif que le fichier n'existe pas déjà
+      return 0;
+    }
+    return 1;
+  }
+
+  //supprime le qcm de nom $id
+  //retourne 0 si la suppression s'est bien passé, 1 sinon
+  public function supprimeQCM($id) {
+    if($this->QCMExiste($id)) {
+      unlink('../app/model/qcm/'.$id.'.xml');
+      return 0;
+    }
+    return 1;
+  }
+
+  //retourne 1 si le qcm existe, 0 sinon
+  public function QCMExiste($id) { 
+    $listeQCM = scandir('../app/model/qcm');
+    foreach($listeQCM as $nomQCM) {
+      if(strcmp($nomQCM, $id.'.xml') == 0) {
+        return 1;
+      }
+    }
+    return 0;
+  }
+
+  
+
 }
 ?>
