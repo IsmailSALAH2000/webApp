602f7b58d30e59e02bb3e4453abb19a7735243ab
Branedon <60399568+Branedon@users.noreply.github.com>
2023-05-04 15:14:29 +0200

    Contrôleur cours

--- app/controllers/Lessons/LessonController.php
+++ app/controllers/Lessons/LessonController.php
@@ -4,15 +4,31 @@ require_once 'Lesson.php'
 require_once '/app/controllers/Sessions.php'
 require '/app/controllers/ViewsLauncher.php'
 
+
+/*
+    Au chargement de la page, on va appeler les fonctions demandées dans la variable POST 'whatToDo', qui représentent des endpoints pour cette page. C'est-à-dire que peu importe le résultat de ces fonctions, une vue sera chargée à leur issue.
+
+    Utilisation : passer les paramètres en POST.
+    'whatToDo' correspond à :
+        - 'addLesson' pour ajouter un cours. Paramètres supplémentaires obligatoires :
+            - 'lesson', correspond à une instance de la classe cours.
+        - 'removeLesson' pour supprimer un cours. Paramètres supplémentaires obligatoires :
+            - 'lessonId', correspond à l'id du cours à supprimer.
+*/
+
 if(isset($_POST['whatToDo']))
 {
     switch($_POST['whatToDo'])
     {
-        case 'AddLesson':
+        case 'addLesson':
             if(!isset($_POST['lesson']))
                 throw new Exception('Impossible d\'ajouter une lesson : paramètre POST "lesson" inexistant.');
             Lessons::AddLEsson($_POST['lesson']);
             break;
+        case 'removeLesson' :
+            if(!isset($_POST['lessonId']))
+                throw new Exception('Impossible de supprimer une lesson : paramètre POST "lessonId" inexistant.');
+            Lessons::RemoveLessonById($_POST['lessonId']);
         default:
             break;
     }
@@ -36,6 +52,17 @@ class Lessons
         else throw new Exception('Aucune session en cours ou utilisateur non admin'); 
        
         ViewsLauncher::LessonAdded();
+    }
+
+    /*
+        Supprime un cours du serveur selon son id;
+    */
+    public static function RemoveLessonById($id)
+    {
+        $coursInstance = new Cours();
+        $coursInstance->supprimerCours($id);
+
+        ViewsLauncher::LessonRemoved();
     }
 
     /*
@@ -66,6 +93,28 @@ class Lessons
 
         return $out;
     }
+
+    /*
+        Retourne un cours selon son ID. Retourne null en cas d'id introuvable.
+    */
+    public static function GetLessonById($id)
+    {
+        $coursInstance = new Cours();
+        $rawLessons = $coursInstance->getCours($id); // Récupération le cours depuis le modèle sous forme brute.
+
+        if($rawLessons == null) return null;
+
+        $lesson = new Lesson(); // On crée une instance de la classe Lesson, permettant une manipulation plus aisée de l'information.
+        $lesson->idCours = $rawLesson['idCours'];
+        $lesson->titre = $rawLesson['titre'];
+        $lesson->description = $rawLesson['description'];
+        $lesson->type = $rawLesson['type'];
+        $lesson->chemin = $rawLesson['chemin'];
+        $lesson->dateCours = $rawLesson['dateCours'];
+
+        return $lesosn;
+    }
+
 }
 
 ?>
