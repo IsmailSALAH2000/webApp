dd6545c3c13efd75f032362e58ed88aafe46cce0
perr-in <131020624+perr-in@users.noreply.github.com>
2023-05-05 14:51:12 +0200

    changement des chemins en relatif

--- app/vues/composants/header/header.php
+++ app/vues/composants/header/header.php
@@ -7,8 +7,8 @@ function head($titrePage, $lienStylePage){
     $html=<<<HTML
         <title>$titrePage</title>
         <link rel="stylesheet" type="text/css" href="$lienStylePage"/>
-        <link rel="stylesheet" type="text/css" href="/vues/composants/header/header.css"/>
-        <link rel="stylesheet" type="text/css" href="/vues/composants/footer/footer.css"/>
+        <link rel="stylesheet" type="text/css" href="../../composants/header/header.css"/>
+        <link rel="stylesheet" type="text/css" href="../../composants/footer/footer.css"/>
         <meta charset="UTF-8"/>
         <meta name="author" content="BEAUJON Alexandre, MALOIGNE Anthony, PERE Brandon, ROBINET Perrine, SALAH Ismail, STRAINCHAMPS Clothilde" />
     HTML;
@@ -20,18 +20,18 @@ function navBar($c){
     $connecter=function($c){
         if(!$c){
             $html=<<<HTML
-                <li><a href="/vues/pages/connexion/connexion.php">COURS</a></li>
-                <li><a href="/vues/pages/connexion/connexion.php">QCM</a></li>
-                <li><a href="/vues/pages/connexion/connexion.php">FORUM</a></li>
-                <li><a href="/vues/pages/connexion/connexion.php">SE CONNECTER</a></li>
+                <li><a href="../../pages/connexion/connexion.php">COURS</a></li>
+                <li><a href="../../pages/connexion/connexion.php">QCM</a></li>
+                <li><a href="../../pages/connexion/connexion.php">FORUM</a></li>
+                <li><a href="../../pages/connexion/connexion.php">SE CONNECTER</a></li>
             HTML;
             return $html;
         }
         else{
             $html=<<<HTML
-                <li><a href="/vues/pages/cours/cours.php">COURS</a></li>
-                <li><a href="/vues/pages/qcm/qcm.php">QCM</a></li>
-                <li><a href="/vues/pages/forum/forum.php">FORUM</a></li>
+                <li><a href="../../pages/cours/cours.php">COURS</a></li>
+                <li><a href="../../pages/qcm/qcm.php">QCM</a></li>
+                <li><a href="../../pages/forum/forum.php">FORUM</a></li>
                 <li><a href="#" class="btnConnection">USER ID</a></li>
                 <!-- <ul class="menuConnection">
                     <li><a href="#" class="choixMenu">Paramètres</a></li>
@@ -51,7 +51,7 @@ function navBar($c){
                         {$connecter($c)}
                     </ul>
                 </div>
-                <img src="/vues/composants/header/menu-btn.png" alt="menu" class="menu-hamburger">
+                <img src="../../composants/header/menu-btn.png" alt="menu" class="menu-hamburger">
             </nav>
         </header>
     HTML;
