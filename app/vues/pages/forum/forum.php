5899ac67d5bce5265447ad6db680b55e7bb48b68
perr-in <131020624+perr-in@users.noreply.github.com>
2023-05-06 17:06:27 +0200

    amélioration du forum, css pas encore fait

--- app/vues/pages/forum/forum.php
+++ app/vues/pages/forum/forum.php
@@ -3,40 +3,77 @@ include('../../composants/header/header.php');
 include('../../composants/footer/footer.php');
 include('../../../controllers/Users/Sessions.php');
 include '../../../controllers/Forum/ForumController.php';
-//include('../../../controllers/Forum/ForumDataStructures.php');
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
 
     <head>
-        <?php head('Accueil', '../../pages/accueil/accueil.css');?>
+        <?php head('Forum', '../../pages/forum/forum.css');?>
     </head>
 
     <body>
         <header>
             <?php 
             $isLogged = Session::Exists();
-            navBar($isLogged); ?>
+            navBar($isLogged); 
+            $user = Session::GetLogin();
+            ?>
         </header>
 
         <main>
+            <h1 class="titrePage">Bienvenue sur le forum !</h1>
+            <h3 class="sous-titrePage">Créer un topic.</h3>
+
+            <form method="post">
+                <input type="hidden" name="whatToDo" value="addTopic">
+                <input type="hidden" name="creatorLogin" value="<?php echo $user; ?>">
+                <input type="text" name="title" placeholder="Titre de votre post" required>
+                <input type="text" name="firstMessageContent" placeholder="Ecrivez votre message." required>
+                <button type="submit">Poster</button>
+            </form>
+
+            <h3 class="sous-titrePage">Voir les topics existants.</h3>
+
             <?php 
                 if($isLogged){
                     $isAdmin = Session::IsAdmin();
                     $listeTopics = ForumController::GetAllTopicHeaders();
                     for ($i=0; $i < count($listeTopics) ; $i++) { 
                         $titre = htmlspecialchars($listeTopics[$i]->title);
+                        $creator = htmlspecialchars($listeTopics[$i]->creator);
+                        $answerNumber = htmlspecialchars($listeTopics[$i]->answersNumber);
+                        $id = htmlspecialchars($listeTopics[$i]->id);
                         $html=<<<HTML
                             <a href="#">
                                 <div class="topic-header">
-                                    <div class="titre-topic">
-                                            $titre
+                                    <div class="titre-topic">$titre</div>
+                                    <div class="bottom-topic-header">
+                                        <div class="creator-topic">$creator</div>
+                                        <div class="nb-answer-topic">$answerNumber</div>
                                     </div>
-                                </div>
-                            </a>
                         HTML;
                         echo $html;
+                        
+                        if ($isAdmin) {
+                            $html=<<<HTML
+                                    <form method="post">
+                                        <input type="hidden" name="whatToDo" value="removeTopic">
+                                        <input type="hidden" name="id" value='{$id}'>
+                                        <button type="submit">Supprimer</button>
+                                    </form>
+                                </div>
+                            </a>
+                            HTML;
+                            echo $html;
+                        }
+                        else{
+                            $html=<<<HTML
+                                </div>
+                            </a>
+                            HTML;
+                            echo $html;
+                        }
                     }
                 }
             ?>
