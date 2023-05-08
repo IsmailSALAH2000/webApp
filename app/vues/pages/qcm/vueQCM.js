14395d5d9e92f90f503be6781a7de76efc30bbe1
perr-in <131020624+perr-in@users.noreply.github.com>
2023-05-08 18:00:56 +0200

    Delete remove buttons

--- app/vues/pages/qcm/vueQCM.js
+++ app/vues/pages/qcm/vueQCM.js
@@ -5,30 +5,17 @@ $(document).ready(function() {
     // Ajouter une nouvelle question
     $("#addQuestion").click(function() {
         questionIndex++;
-        var newQuestion = '<div class="question"><label name>Question ' + questionIndex + '</label><input type="text" name="question' + questionIndex + '"><button type="button" class="removeQuestion">Supprimer la question</button><button type="button" class="addReponse">Ajouter une réponse</button><div class="reponses"></div></div>';
+        var newQuestion = '<div class="question"><label name>Question ' + questionIndex + '</label><input type="text" name="question' + questionIndex + '"><button type="button" class="addReponse">Ajouter une réponse</button><div class="reponses"></div></div>';
         $("#editQCM").append(newQuestion);
         reponseIndex=0;
-    });
-
-    // Supprimer une question
-    $("#editQCM").on("click", ".removeQuestion", function() {
-        $(this).closest(".question").remove();
-        questionIndex--;
     });
 
     // Ajouter une nouvelle réponse
     $("#editQCM").on("click", ".addReponse", function() {
         reponseIndex++;
         var parentQuestion = $(this).closest(".question");
-        var newReponse = '<div class="reponse"><label>Réponse ' + reponseIndex + '</label><input type="text" name="reponse' + questionIndex + reponseIndex +'"><input type="checkbox" class="checkbox"name="correction' + questionIndex +reponseIndex + '" value="true"> Vrai <input type="checkbox" class="checkbox"name="correction' + questionIndex + reponseIndex + '" value=""> Faux <button type="button" class="removeReponse">Supprimer la réponse</button></div>';
+        var newReponse = '<div class="reponse"><label>Réponse: </label><input type="text" name="reponse' + questionIndex + reponseIndex +'"><input type="radio" class="checkbox"name="correction' + questionIndex +reponseIndex + '" value="true"> Vrai <input type="radio" class="checkbox"name="correction' + questionIndex + reponseIndex + '" value="false" checked="true"> Faux </div>';
         $(parentQuestion).find(".reponses").append(newReponse);
     });
-
-    // Supprimer une réponse
-    $("#editQCM").on("click", ".removeReponse", function() {
-        $(this).closest(".reponse").remove();
-        reponseIndex--;
-    });
-
 
 });
