a3f2ef95d8f366df82b3c9ddf36024fa09f755b3
hitanox <102423395+hitanox@users.noreply.github.com>
2023-05-08 16:23:32 +0200

    Add files via upload

--- app/model/Model.php
+++ app/model/Model.php
@@ -4,22 +4,15 @@ require_once __DIR__ . '/Database.php';
 
 trait Model {
     use Database;
-    
-    //protected $limit=5;
-    //protected $offset=0;
-    //protected $order_type = "desc";
-    //protected $order_column = "id";
     public $errors = [];
 
 
     public function findAll(){
-        $query = "select * from $this->table"; //order by $this->order_column $this->order_type limit $this->limit offset $this->offset";
+        $query = "select * from $this->table order by 1";
         return $this->query($query);
     }
 
     public function selectAll($data, $data_not=[]){
-
-        $query = "";
 
         $query = "select * from $this->table where ";
         $keys = array_keys($data);
@@ -34,16 +27,15 @@ trait Model {
         }
 
         $query = trim($query, " && ");
+        $query .= ' order by 1'; 
 
-        //$query .= " order by $this->order_column $this->order_type limit $this->limit offset $this->offset";
         $data = array_merge($data, $data_not);
 
         return $this->query($query, $data);
 
     }
 
-    public function select($data, $data_not=[]){ //ok
-        //$query = "";
+    public function select($data, $data_not=[]){
 
         $query = "select * from $this->table where ";
         $keys = array_keys($data);
@@ -58,8 +50,6 @@ trait Model {
         }
 
         $query = trim($query, " &");//supprime les ' ' et '&' en début et fin de chaine //$query = trim($query, " && ");
-
-        //$query .= " limit $this->limit offset $this->offset";
         $data = array_merge($data, $data_not);
 
         $result = $this->query($query, $data);
@@ -68,43 +58,18 @@ trait Model {
         return false;
     }
 
-    public function insert($data){ //ok
-
-        /*
-        if (!empty($this-> allowedColums)){
-            foreach ($data as $key => $value) {
-                if(!in_array($key, $this->allowedColums)){
-                    unset($data[$key]); 
-                }
-            }
-        }*/
-
-        //$query = "";
+    public function insert($data){
         $keys = array_keys($data);
         $query = "insert into $this->table (" . implode(", ",$keys) . ") values (:" . implode(", :",$keys) . ") ";
         return $this->query($query, $data);
     }
 
-    public function update($idValue, $data, $idName){ //ok
-        /*
-        //annuler les entrées non obligatoire -> tout ce qui n'est pas obligatoire sur un formulaire ne doit pas être envoyer dans le tableau $data
-        if (!empty($this-> allowedColums)){
-            foreach ($data as $key => $value) {
-                if(!in_array($key, $this->allowedColums)){
-                    unset($data[$key]); 
-                }
-            }
-        }
-
-        $query = "";
-*/
+    public function update($idValue, $data, $idName){
         $query = "update $this->table set ";
         $keys = array_keys($data);
-
         foreach ($keys as $key) {
             $query .= $key . " = :" . $key . " , "; 
         }
-
         $query = trim($query, " ,");
         $query .= " where $idName = :$idName";
         $data[$idName] = $idValue;
@@ -112,7 +77,6 @@ trait Model {
     }
 
     public function delete($id, $idName){
-        //$query = "";
         $data[$idName] = $id;
         $query = "delete from $this->table where $idName = :$idName";
         return $this->query($query, $data);
